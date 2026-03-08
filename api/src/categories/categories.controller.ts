47a6973e4710e0724b075602038f3056d6e8ccb1
vincent-guizot <vincentguizot128@gmail.com>
2026-03-08 20:44:04 +0700

    categories

--- api/src/categories/categories.controller.ts
+++ api/src/categories/categories.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+   Controller,
+   Get,
+   Post,
+   Body,
+   Param,
+   Patch,
+   Delete,
+   Query
+} from '@nestjs/common'
+
+import { CategoriesService } from './categories.service'
+import { CreateCategoryDto } from './dto/create-category.dto'
+import { UpdateCategoryDto } from './dto/update-category.dto'
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+
+   constructor(private readonly service: CategoriesService) { }
+
+   @Post()
+   create(@Body() body: CreateCategoryDto) {
+      return this.service.create(body)
+   }
+
+   @Get()
+   findAll(@Query('brandId') brandId?: string) {
+
+      if (brandId) {
+         return this.service.findAllByBrand(Number(brandId))
+      }
+
+      return this.service.findAll()
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return this.service.findOne(Number(id))
+   }
+
+   @Patch(':id')
+   update(
+      @Param('id') id: string,
+      @Body() body: UpdateCategoryDto
+   ) {
+      return this.service.update(Number(id), body)
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id: string) {
+      return this.service.remove(Number(id))
+   }
+
+}
--- api/src/categories/categories.service.ts
+++ api/src/categories/categories.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../../prisma/prisma.service'
+import { CreateCategoryDto } from './dto/create-category.dto'
+import { UpdateCategoryDto } from './dto/update-category.dto'
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+
+   constructor(private prisma: PrismaService) { }
+
+   async create(data: CreateCategoryDto) {
+      return this.prisma.category.create({
+         data
+      })
+   }
+
+   async findAll() {
+      return this.prisma.category.findMany({
+         include: {
+            brand: true
+         },
+         orderBy: {
+            id: 'desc'
+         }
+      })
+   }
+
+   async findOne(id: number) {
+      return this.prisma.category.findUnique({
+         where: { id },
+         include: {
+            brand: true
+         }
+      })
+   }
+
+   async findAllByBrand(brandId: number) {
+      return this.prisma.category.findMany({
+         where: { brandId }
+      })
+   }
+
+   async update(id: number, data: UpdateCategoryDto) {
+      return this.prisma.category.update({
+         where: { id },
+         data
+      })
+   }
+
+   async remove(id: number) {
+      return this.prisma.category.delete({
+         where: { id }
+      })
+   }
+
+}
