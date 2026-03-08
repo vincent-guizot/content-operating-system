aa4c6b92f2617224bf3a629f1d269aaea1f7c301
vincent-guizot <vincentguizot128@gmail.com>
2026-03-08 21:06:14 +0700

    media

--- api/src/media/media.controller.ts
+++ api/src/media/media.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+   Controller,
+   Get,
+   Post,
+   Body,
+   Param,
+   Patch,
+   Delete
+} from '@nestjs/common'
+
+import { MediaService } from './media.service'
+import { CreateMediaDto } from './dto/create-media.dto'
+import { UpdateMediaDto } from './dto/update-media.dto'
 
 @Controller('media')
-export class MediaController {}
+export class MediaController {
+
+   constructor(private readonly service: MediaService) { }
+
+   @Post()
+   create(@Body() body: CreateMediaDto) {
+      return this.service.create(body)
+   }
+
+   @Get()
+   findAll() {
+      return this.service.findAll()
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return this.service.findOne(Number(id))
+   }
+
+   @Patch(':id')
+   update(
+      @Param('id') id: string,
+      @Body() body: UpdateMediaDto
+   ) {
+      return this.service.update(Number(id), body)
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id: string) {
+      return this.service.remove(Number(id))
+   }
+
+}
--- api/src/media/media.service.ts
+++ api/src/media/media.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../../prisma/prisma.service'
+import { CreateMediaDto } from './dto/create-media.dto'
+import { UpdateMediaDto } from './dto/update-media.dto'
 
 @Injectable()
-export class MediaService {}
+export class MediaService {
+
+   constructor(private prisma: PrismaService) { }
+
+   async create(data: CreateMediaDto) {
+      return this.prisma.media.create({
+         data
+      })
+   }
+
+   async findAll() {
+      return this.prisma.media.findMany({
+         orderBy: {
+            createdAt: 'desc'
+         }
+      })
+   }
+
+   async findOne(id: number) {
+      return this.prisma.media.findUnique({
+         where: { id }
+      })
+   }
+
+   async update(id: number, data: UpdateMediaDto) {
+      return this.prisma.media.update({
+         where: { id },
+         data
+      })
+   }
+
+   async remove(id: number) {
+      return this.prisma.media.delete({
+         where: { id }
+      })
+   }
+
+}
