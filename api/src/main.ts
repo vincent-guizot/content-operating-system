50e46cf22e8b426cb9daf77f61a2c7385016cc5e
vincent-guizot <vincentguizot128@gmail.com>
2026-03-09 17:46:09 +0700

    FIXING

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe())
 
-  await app.listen(process.env.PORT || 3000)
+  const port = process.env.PORT || 3000
+
+  await app.listen(port)
+
+  console.log(`🚀 Server running on port ${port}`)
 }
 bootstrap()
