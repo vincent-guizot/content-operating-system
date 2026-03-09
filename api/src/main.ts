1aec6d5a56bb6991ad1b5701789af20343e1856b
vincent-guizot <vincentguizot128@gmail.com>
2026-03-09 17:52:41 +0700

    api for render and enable cors yes

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,16 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app/app.module'
 import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule)
 
-  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors()
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true
+    })
+  )
 
   const port = process.env.PORT || 3000
 
   await app.listen(port)
 
-  console.log(`🚀 Server running on port ${port}`)
+  console.log(`🚀 Orange Scrolls API running on port ${port}`)
 }
+
 bootstrap()
